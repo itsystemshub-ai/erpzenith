b7c8d30bef0f9c41a03179fb36ab7d0855f300ff
Elias Tapias <it.systems.hub@gmail.com>
2026-03-25 18:49:54 -0400

    SUBIR

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
-      { protocol: 'https', hostname: 'nmhxxmvvbgqnodlfetil.supabase.co' },
+      { protocol: 'https', hostname: '*.supabase.co' },
     ],
   },
   eslint: {
